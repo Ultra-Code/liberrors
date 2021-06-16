30d20f6d4452a99be23824370a8219e55b6b9f1e
Ultra-Code <mega.alpha100@gmail.com>
2021-06-16 06:09:11 +0000

    Move std::abort into the if conditions scope to prevent it from been called unconditionally
    Remove the noreturn attribute from the prototype and implementation of assert_that

--- liberrors/errors.cxx
+++ liberrors/errors.cxx
@@ -9,66 +9,68 @@
 namespace errors
 {
 
-namespace
-{
-  auto
-  format_error(std::string_view id_error) noexcept
+  namespace
   {
-    return [id_error](std::string_view msg,
-                      std::source_location loc) -> std::string {
-      try
-        {
-
-          auto style = fg(fmt::color::crimson) | fmt::emphasis::bold;
-          auto assert_fmt
-              = fmt::format(style, FMT_STRING(" {} : {}\n"), id_error, msg);
-          auto loc_info
-              = fmt::format(FMT_COMPILE("@ in file:{} at line {}:{}\n"),
-                            loc.file_name(), loc.line(), loc.column());
-          auto func_proto = fmt::format(FMT_COMPILE("in function {}\n"),
-                                        loc.function_name());
-          auto bt_style = fmt::emphasis::bold;
-          auto backtrace = fmt::format(
-              bt_style,FMT_STRING("with backtrace:\n{}\n"),
-              boost::stacktrace::to_string(boost::stacktrace::stacktrace()));
-          return fmt::format(FMT_COMPILE("{} {} {} {}"), assert_fmt, loc_info,
-                             func_proto, backtrace);
-        }
-      catch (fmt::format_error const & fmt_err)
-        {
-            constexpr auto loc=std::source_location::current();
-            std::cerr<<"Format error in "<<loc.file_name()<<"\n"
-                <<"@ line "<<loc.line()<<" : "<<loc.column()<<" of "<<loc.function_name()<<'\n';
-            std::cerr<<"With message : "<<fmt_err.what()<<'\n';
-            return "Error occurred when formating error string\n";
-        }
-    };
-  }
-  const auto format_assertion = format_error("Assertion failed");
-  const auto format_exception = format_error("Exception thrown with message");
-
-} // namespace
-
-[[noreturn]] auto
-assert_that(bool condition, std::string_view assert_msg,
-            std::source_location loc) noexcept -> void
-{
-  if (not condition)
+    auto
+    format_error(std::string_view id_error) noexcept
     {
-      std::cerr << format_assertion(assert_msg, loc);
+      return [id_error](std::string_view msg,
+                        std::source_location loc) -> std::string {
+        try
+          {
+
+            auto style = fg(fmt::color::crimson) | fmt::emphasis::bold;
+            auto assert_fmt
+                = fmt::format(style, FMT_STRING(" {} : {}\n"), id_error, msg);
+            auto loc_info
+                = fmt::format(FMT_COMPILE("@ in file:{} at line {}:{}\n"),
+                              loc.file_name(), loc.line(), loc.column());
+            auto func_proto = fmt::format(FMT_COMPILE("in function {}\n"),
+                                          loc.function_name());
+            auto bt_style = fmt::emphasis::bold;
+            auto backtrace = fmt::format(
+                bt_style, FMT_STRING("with backtrace:\n{}\n"),
+                boost::stacktrace::to_string(boost::stacktrace::stacktrace()));
+            return fmt::format(FMT_COMPILE("{} {} {} {}"), assert_fmt,
+                               loc_info, func_proto, backtrace);
+          }
+        catch (fmt::format_error const &fmt_err)
+          {
+            constexpr auto loc = std::source_location::current();
+            std::cerr << "Format error in " << loc.file_name() << "\n"
+                      << "@ line " << loc.line() << " : " << loc.column()
+                      << " of " << loc.function_name() << '\n';
+            std::cerr << "With message : " << fmt_err.what() << '\n';
+            return "Error occurred when formating error string\n";
+          }
+      };
     }
-  std::abort();
-}
+    const auto format_assertion = format_error("Assertion failed");
+    const auto format_exception
+        = format_error("Exception thrown with message");
 
-recoverable_err::recoverable_err(std::string_view msg,
-                             std::source_location loc) noexcept
-    : message{ format_exception(msg, loc) }
-{
-}
+  } // namespace
 
-auto
-recoverable_err::what() const noexcept -> const char *
-{
-  return message.c_str();
-}
-}  // namespace errors
+   auto
+  assert_that(bool condition, std::string_view assert_msg,
+              std::source_location loc) noexcept -> void
+  {
+    if (not condition)
+      {
+        std::cerr << format_assertion(assert_msg, loc);
+        std::abort();
+      }
+  }
+
+  recoverable_err::recoverable_err(std::string_view msg,
+                                   std::source_location loc) noexcept
+      : message{ format_exception(msg, loc) }
+  {
+  }
+
+  auto
+  recoverable_err::what() const noexcept -> const char *
+  {
+    return message.c_str();
+  }
+} // namespace errors
--- liberrors/errors.hxx
+++ liberrors/errors.hxx
@@ -1,3 +1,6 @@
+/**
+ * @file errors.hxx
+ */
 #pragma once
 #include <source_location>
 #include <string_view>
@@ -5,10 +8,23 @@
 
 #include <liberrors/export.hxx>
 
+/**
+ * @namespace errors
+ * @brief namespace containing functions for error handling
+ */
 namespace errors
 {
+  /**
+   * @brief function to assert that @param condition is true else failfast with
+   * the error message @param assert_msg
+   *
+   * @param [in] condition to check
+   * @param [in] assert_msg :message to print to stderr when @param condition
+   * is false
+   * @returns void
+   */
 
-  [[noreturn]] auto
+  auto
   assert_that(bool condition, std::string_view assert_msg,
               std::source_location loc
               = std::source_location::current()) noexcept -> void;
@@ -22,8 +38,8 @@ namespace errors
 
   public:
     explicit recoverable_err(std::string_view msg,
-                           std::source_location loc
-                           = std::source_location::current()) noexcept;
+                             std::source_location loc
+                             = std::source_location::current()) noexcept;
     [[nodiscard]] auto
     what() const noexcept -> const char * override;
   };
