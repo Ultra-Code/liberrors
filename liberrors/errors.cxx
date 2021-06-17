c5392bbf40941a17e78af33f02b4df2aaede196f
Ultra-Code <mega.alpha100@gmail.com>
2021-06-17 02:55:21 +0000

    README.md

--- liberrors/errors.cxx
+++ liberrors/errors.cxx
@@ -19,20 +19,24 @@ namespace errors
         try
           {
 
-            auto style = fg(fmt::color::crimson) | fmt::emphasis::bold;
-            auto assert_fmt
-                = fmt::format(style, FMT_STRING(" {} : {}\n"), id_error, msg);
-            auto loc_info
-                = fmt::format(FMT_COMPILE("@ in file:{} at line {}:{}\n"),
-                              loc.file_name(), loc.line(), loc.column());
-            auto func_proto = fmt::format(FMT_COMPILE("in function {}\n"),
-                                          loc.function_name());
-            auto bt_style = fmt::emphasis::bold;
+            auto assert_style = fg(fmt::color::crimson) | fmt::emphasis::bold;
+            auto assert_fmt = fmt::format(
+                assert_style, FMT_STRING(" {} : {}\n"), id_error, msg);
+            auto loc_style = fmt::fg(fmt::color::forest_green);
+            auto loc_info = fmt::format(loc_style, FMT_STRING("@ in file:{}"),
+                                        loc.file_name());
+            auto line_info = fmt::format(FMT_STRING(" at line {}:{}\n"),
+                                         loc.line(), loc.column());
+            auto func_proto_style = fmt::fg(fmt::color::yellow);
+            auto func_proto
+                = fmt::format(func_proto_style, FMT_STRING("in function {}\n"),
+                              loc.function_name());
+            auto bt_style = fmt::fg(fmt::color::alice_blue);
             auto backtrace = fmt::format(
                 bt_style, FMT_STRING("with backtrace:\n{}\n"),
                 boost::stacktrace::to_string(boost::stacktrace::stacktrace()));
-            return fmt::format(FMT_COMPILE("{} {} {} {}"), assert_fmt,
-                               loc_info, func_proto, backtrace);
+            return fmt::format(FMT_COMPILE("{} {} {} {} {}"), assert_fmt,
+                               loc_info, line_info, func_proto, backtrace);
           }
         catch (fmt::format_error const &fmt_err)
           {
@@ -51,7 +55,7 @@ namespace errors
 
   } // namespace
 
-   auto
+  auto
   assert_that(bool condition, std::string_view assert_msg,
               std::source_location loc) noexcept -> void
   {
