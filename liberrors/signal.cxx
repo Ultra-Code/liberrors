203635366f1891404fc28ae27126ec06fe20e076
Ultra-Code <mega.alpha100@gmail.com>
2021-07-12 22:33:15 +0000

    Use backward for common signal handling

--- liberrors/signal.cxx
+++ liberrors/signal.cxx
@@ -1,26 +1,8 @@
-#include <csignal>
-#include <boost/stacktrace.hpp>
-#include <iostream>
+#define BACKWARD_HAS_DW 1
+#include <backward.hpp>
 
-using boost::stacktrace::stacktrace;
+namespace backward {
 
-namespace errors
-{
-  namespace
-  {
-    auto
-    signal_handler(int sig_num) -> void
-    {
-      // set sig_num to it's default signal handling
-      std::signal(sig_num, SIG_DFL);
-      std::cerr << stacktrace();
-      std::raise(SIGABRT);
-    }
-  } // namespace
-  auto
-  trace_on_abort() -> void
-  {
-    std::signal(SIGSEGV, &signal_handler);
-    std::signal(SIGABRT, &signal_handler);
-  }
-} // namespace errors
+backward::SignalHandling sh;
+
+} // namespace backward
